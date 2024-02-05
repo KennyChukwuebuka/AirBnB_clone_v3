54c041dd73c4f696b3987b96dec0fdb86bb7147d
kennyChukwuebuka <kenkinado@gmail>
2024-02-05 21:39:04 +0100

    task Api working

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,18 +6,16 @@ import app_views from api.v1.views
 register the blueprint app_views to your Flask instance app
 declare a method to handle @app.teardown_appcontext
 that calls storage.close()
-inside if __name__ == "__main__":, run your
-Flask server (variable app) with:
-    host = environment variable HBNB_API_HOST
-    or 0.0.0.0 if not defined
-    port = environment variable HBNB_API_PORT or
-    5000 if not defined
-    threaded=True
+inside if __name__ == "__main__":, run your Flask server (variable app) with:
+host = environment variable HBNB_API_HOST or 0.0.0.0 if not defined
+port = environment variable HBNB_API_PORT or 5000 if not defined
+threaded=True
 """
 
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
+from os import getenv
 
 
 app = Flask(__name__)
@@ -30,5 +28,13 @@ def teardown_db(exception):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,13 +6,10 @@ that returns a JSON: "status": "OK"
 """
 
 from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def status():
-    """returns a JSON: "status": "OK" """
-    return {"status": "OK"}
-
-
-if __name__ == "__main__":
-    app_views.run(host="0.0.0.0", port="5000")
+    ''' routes to status page '''
+    return jsonify({'status': 'OK'})
