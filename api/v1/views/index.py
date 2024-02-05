954b579e5355b07774f1840bf677e9f976ad154e
kennyChukwuebuka <kenkinado@gmail>
2024-02-05 14:59:53 +0100

    added api

---
+++ api/v1/views/index.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+import app_views from api.v1.views
+create a route /status on the object app_views
+that returns a JSON: "status": "OK"
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    ''' routes to status page '''
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def count():
+    '''retrieves the number of each objects by type'''
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
