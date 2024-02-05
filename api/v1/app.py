ecc6be1504b0c89817922d9f9dbaf92b97003f42
kennyChukwuebuka <kenkinado@gmail>
2024-02-05 23:19:38 +0100

    addedcors

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,7 +22,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
-app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
